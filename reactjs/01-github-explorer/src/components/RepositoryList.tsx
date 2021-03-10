2285721aac30e6fd7bd838016f44ccc34d6168aa
Yuri Alves <yuripiresalves@gmail.com>
2021-03-10 11:28:34 -0300

    add typescript

--- reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,10 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-// https://api.github.com/users/yuripiresalves/repos
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/users/yuripiresalves/repos')

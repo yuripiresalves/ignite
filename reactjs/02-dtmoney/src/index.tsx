b9bb86992744595ac6170406dfeee08c000d93bf
Yuri Alves <yuripiresalves@gmail.com>
2021-03-18 16:12:31 -0300

    insert transaction on api

--- reactjs/02-dtmoney/src/index.tsx
+++ reactjs/02-dtmoney/src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-03-18 15:56:55'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1500,
+          createdAt: new Date('2021-03-10 14:00:55'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 

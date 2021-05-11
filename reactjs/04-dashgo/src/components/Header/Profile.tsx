a3ee1f56fa14a591db4a129edde2ae63973344b6
Yuri Alves <yuripiresalves@gmail.com>
2021-05-11 13:52:52 -0300

    add responsive Header

--- reactjs/04-dashgo/src/components/Header/Profile.tsx
+++ reactjs/04-dashgo/src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Yuri Alves</Text>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Yuri Alves</Text>
 
-        <Text color="gray.300" fontSize="small">
-          yuripiresalves@gmail.com
-        </Text>
-      </Box>
+          <Text color="gray.300" fontSize="small">
+            yuripiresalves@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"

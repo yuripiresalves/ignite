2b0b412dd6b2c626d01f69cf425726933f2bdaee
Yuri Alves <yuripiresalves@gmail.com>
2022-09-19 18:06:57 -0300

    create Sidebar component

--- reactjs-2022/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-2022/01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,9 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
+
 import './global.css'
 
 function App() {
@@ -7,7 +11,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Post author="Yuri" content="lorem ipsum " />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Yuri" content="lorem ipsum " />
+          <Post author="Yuri" content="lorem ipsum " />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ reactjs-2022/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+      <div className={styles.profile}>
+        <strong>Yuri Alves</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}

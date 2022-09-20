78bc0af522c997389ae89ea1ca42895fac28ebc4
Yuri Alves <yuripiresalves@gmail.com>
2022-09-20 11:12:12 -0300

    finish sidebar component

--- reactjs-2022/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ reactjs-2022/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,12 +7,14 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/yuripiresalves.png" />
         <strong>Yuri Alves</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>

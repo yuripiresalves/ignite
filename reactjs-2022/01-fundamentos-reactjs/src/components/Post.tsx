efaa6049b96ea117056bd268338ff79205f11f37
Yuri Alves <yuripiresalves@gmail.com>
2022-10-24 16:15:58 -0300

    Add Post component

---
+++ reactjs-2022/01-fundamentos-reactjs/src/components/Post.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/yuripiresalves.png"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>Yuri Alves</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="24 de outubro às 14:30" dateTime="2022-10-24 14:40:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto </a>
+          <a href="">#nlw </a>
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}

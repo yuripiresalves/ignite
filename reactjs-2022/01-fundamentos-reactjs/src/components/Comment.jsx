a173a12c95f0bc2c304f08e26290bc7232f34879
Yuri Alves <yuripiresalves@gmail.com>
2022-10-28 00:27:28 -0300

    finish app functions

--- reactjs-2022/01-fundamentos-reactjs/src/components/Comment.jsx
+++ reactjs-2022/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,38 +1,56 @@
-import { ThumbsUp, Trash } from 'phosphor-react'
-import { Avatar } from './Avatar'
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
 
-import styles from './Comment.module.css'
+import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1);
+  }
+
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/yuripiresalves.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/yuripiresalves.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Yuri Alves</strong>
-              <time title="24 de outubro às 14:30" dateTime="2022-10-24 14:40:30">
+              <time
+                title="24 de outubro às 14:30"
+                dateTime="2022-10-24 14:40:30"
+              >
                 Cerca de 1h atrás
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom Devon, Parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
     </div>
-  )
+  );
 }

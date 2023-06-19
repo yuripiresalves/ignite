1b7f845ccf181e601b5ee69d95d744ae3f4f0749
Yuri Alves <yuripiresalves@gmail.com>
2023-06-19 09:30:08 -0300

    add docs workflow

--- reactjs-2022/05-design-system/packages/docs/.storybook/main.js
+++ reactjs-2022/05-design-system/packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ const config = {
   docs: {
     autodocs: 'tag',
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/';
+    }
+
+    return config;
+  },
 };
 export default config;

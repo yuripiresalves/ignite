addc5add735ff77e4e02b70810b004ea079b4501
Yuri Alves <yuripiresalves@gmail.com>
2023-06-22 03:53:12 -0300

    atualizacao do perfil

---
+++ reactjs-2022/06-ignite-call/src/pages/register/update-profile/index.page.tsx
@@ -0,0 +1,98 @@
+import {
+  Avatar,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextArea,
+} from '@ignite-ui/react';
+import { Container, Header } from '../styles';
+import { ArrowRight } from 'phosphor-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormAnnotaion, ProfileBox } from './styles';
+import { useSession } from 'next-auth/react';
+import { GetServerSideProps } from 'next';
+import { getServerSession } from 'next-auth';
+import { buildNextAuthOptions } from '@/pages/api/auth/[...nextauth].api';
+import { api } from '@/lib/axios';
+import { useRouter } from 'next/router';
+
+const UpdateProfileSchema = z.object({
+  bio: z.string(),
+});
+
+type UpdateProfileData = z.infer<typeof UpdateProfileSchema>;
+
+export default function UpdateProfile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<UpdateProfileData>({
+    resolver: zodResolver(UpdateProfileSchema),
+  });
+
+  const session = useSession();
+  const router = useRouter();
+
+  async function handleUpdateProfile(data: UpdateProfileData) {
+    await api.put('/users/profile', {
+      bio: data.bio,
+    });
+
+    await router.push(`/schedule/${session.data?.user.username}`);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as='strong'>Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={4} />
+      </Header>
+
+      <ProfileBox as='form' onSubmit={handleSubmit(handleUpdateProfile)}>
+        <label>
+          <Text size='sm'>Foto de perfil</Text>
+          <Avatar
+            src={session.data?.user.avatar_url}
+            alt={session.data?.user.name}
+          />
+        </label>
+
+        <label>
+          <Text size='sm'>Sobre você</Text>
+          <TextArea {...register('bio')} />
+          <FormAnnotaion size='sm'>
+            Fala um pouco sobre você. Isto será exibido em sua página pessoal.
+          </FormAnnotaion>
+        </label>
+
+        <Button type='submit' disabled={isSubmitting}>
+          Finalizar
+          <ArrowRight />
+        </Button>
+      </ProfileBox>
+    </Container>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res)
+  );
+
+  return {
+    props: {
+      session,
+    },
+  };
+};

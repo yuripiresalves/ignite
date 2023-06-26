e7f60af6df3276f7cf52c3dcb5e6c8b6ecff9d9b
Yuri Alves <yuripiresalves@gmail.com>
2023-06-26 11:51:34 -0300

    calendario e disponibilidade

---
+++ reactjs-2022/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { api } from '@/lib/axios';
+import dayjs from 'dayjs';
+import { Calendar } from '@/components/Calendar';
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles';
+import { useQuery } from '@tanstack/react-query';
+
+interface Availability {
+  possibleTimes: number[];
+  availableTimes: number[];
+}
+
+export function CalendarStep() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const router = useRouter();
+
+  const isDateSelected = !!selectedDate;
+  const username = String(router.query.username);
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null;
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null;
+
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null;
+
+  const { data: availability } = useQuery<Availability>(
+    ['availability', selectedDateWithoutTime],
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
+        params: {
+          date: selectedDateWithoutTime,
+        },
+      });
+
+      return response.data;
+    },
+    {
+      enabled: !!selectedDate,
+    }
+  );
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            {weekDay}, <span>{describedDate}</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            {availability?.possibleTimes.map((hour) => (
+              <TimePickerItem
+                key={hour}
+                disabled={!availability.availableTimes.includes(hour)}
+              >
+                {String(hour).padStart(2, '0')}:00
+              </TimePickerItem>
+            ))}
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  );
+}
---
+++ reactjs-2022/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,73 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react';
+import { ConfirmForm, FormActions, FormError, FormHeader } from './styles';
+import { CalendarBlank, Clock } from 'phosphor-react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const confirmFormSchema = z.object({
+  name: z.string().min(3, { message: 'O nome precisa no mínimo 3 caracteres' }),
+  email: z.string().email({ message: 'Digite um e-mail válido' }),
+  observations: z.string().nullable(),
+});
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>;
+
+export function ConfirmStep() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(confirmFormSchema),
+  });
+
+  function handleConfirmScheduling(data: ConfirmFormData) {}
+
+  return (
+    <ConfirmForm as='form' onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          23 de junho de 2023
+        </Text>
+        <Text>
+          <Clock />
+          15:00
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size='sm'>Nome completo</Text>
+        <TextInput placeholder='Seu nome' {...register('name')} />
+        {errors.name && <FormError size='sm'>{errors.name.message}</FormError>}
+      </label>
+
+      <label>
+        <Text size='sm'>Endereço de e-mail</Text>
+        <TextInput
+          type='email'
+          placeholder='johndoe@example.com'
+          {...register('email')}
+        />
+        {errors.email && (
+          <FormError size='sm'>{errors.email.message}</FormError>
+        )}
+      </label>
+
+      <label>
+        <Text size='sm'>Observações</Text>
+        <TextArea {...register('observations')} />
+      </label>
+
+      <FormActions>
+        <Button type='button' variant='tertiary'>
+          Cancelar
+        </Button>
+        <Button type='submit' disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  );
+}

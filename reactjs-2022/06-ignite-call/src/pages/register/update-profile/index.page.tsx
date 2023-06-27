d8ce4d54d863f92c927d7f33c42d7301dcdfb44e
Yuri Alves <yuripiresalves@gmail.com>
2023-06-27 09:28:17 -0300

    deploy

--- reactjs-2022/06-ignite-call/src/pages/register/update-profile/index.page.tsx
+++ reactjs-2022/06-ignite-call/src/pages/register/update-profile/index.page.tsx
@@ -18,6 +18,7 @@ import { getServerSession } from 'next-auth';
 import { buildNextAuthOptions } from '@/pages/api/auth/[...nextauth].api';
 import { api } from '@/lib/axios';
 import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
 
 const UpdateProfileSchema = z.object({
   bio: z.string(),
@@ -46,40 +47,44 @@ export default function UpdateProfile() {
   }
 
   return (
-    <Container>
-      <Header>
-        <Heading as='strong'>Bem-vindo ao Ignite Call!</Heading>
-        <Text>
-          Precisamos de algumas informações para criar seu perfil! Ah, você pode
-          editar essas informações depois.
-        </Text>
+    <>
+      <NextSeo title='Atualize seu perfil | Ignite Call' noindex />
 
-        <MultiStep size={4} currentStep={4} />
-      </Header>
+      <Container>
+        <Header>
+          <Heading as='strong'>Bem-vindo ao Ignite Call!</Heading>
+          <Text>
+            Precisamos de algumas informações para criar seu perfil! Ah, você
+            pode editar essas informações depois.
+          </Text>
 
-      <ProfileBox as='form' onSubmit={handleSubmit(handleUpdateProfile)}>
-        <label>
-          <Text size='sm'>Foto de perfil</Text>
-          <Avatar
-            src={session.data?.user.avatar_url}
-            alt={session.data?.user.name}
-          />
-        </label>
+          <MultiStep size={4} currentStep={4} />
+        </Header>
 
-        <label>
-          <Text size='sm'>Sobre você</Text>
-          <TextArea {...register('bio')} />
-          <FormAnnotaion size='sm'>
-            Fala um pouco sobre você. Isto será exibido em sua página pessoal.
-          </FormAnnotaion>
-        </label>
+        <ProfileBox as='form' onSubmit={handleSubmit(handleUpdateProfile)}>
+          <label>
+            <Text size='sm'>Foto de perfil</Text>
+            <Avatar
+              src={session.data?.user.avatar_url}
+              alt={session.data?.user.name}
+            />
+          </label>
 
-        <Button type='submit' disabled={isSubmitting}>
-          Finalizar
-          <ArrowRight />
-        </Button>
-      </ProfileBox>
-    </Container>
+          <label>
+            <Text size='sm'>Sobre você</Text>
+            <TextArea {...register('bio')} />
+            <FormAnnotaion size='sm'>
+              Fala um pouco sobre você. Isto será exibido em sua página pessoal.
+            </FormAnnotaion>
+          </label>
+
+          <Button type='submit' disabled={isSubmitting}>
+            Finalizar
+            <ArrowRight />
+          </Button>
+        </ProfileBox>
+      </Container>
+    </>
   );
 }
 

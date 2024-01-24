4141fa937738859f107b8426131142cee4283106
Yuri Alves <yuripiresalves@gmail.com>
2024-01-24 12:09:56 -0300

    initial configs

---
+++ bonus/devstore/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
+export const metadata: Metadata = {
+  title: 'devstore',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html className={inter.variable} lang="pt">
+      <body>{children}</body>
+    </html>
+  )
+}
